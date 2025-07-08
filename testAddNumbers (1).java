26b6be5c3975d415fed93107c195a09dc3e54dcb
PraveenReddy79 <112690643+PraveenReddy79@users.noreply.github.com>
2025-07-08 17:04:00 +0530

    Update testAddNumbers (1).java

--- testAddNumbers (1).java
+++ testAddNumbers (1).java
@@ -5,12 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class testAddNumbers {
+	jUnitFunctions Junit = new jUnitFunctions();
 
 	@Test
 	public void test() {
-		jUnitFunctions Junit = new jUnitFunctions();
 		int result = Junit.testAddNumbers(100,200);
 		assertEquals(300,result);
 	}
+	@Test
+	public void test2(){
+		String res = Junit.addString("Hello", "World");
+		assertEquals("HelloWorld", res);
 
 }
